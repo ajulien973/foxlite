8efc0f13fe320f02d8efa2c620b166abbca519cd
Alexis Julien <ajulien@octo.com>
2019-08-29 05:26:00 +0200

    chore: add simple api server with no route

---
+++ src/api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(3001, () => {
+  console.log('listening to http://localhost:3001');
+});
